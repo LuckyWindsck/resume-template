217169de40d54913b6fb66968f1247a1b1cce533
LuckyWind_sck <luckywindsck@gmail.com>
2023-08-05 18:18:05 +0900

    build: update tailwindcss & postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js}'],
   theme: {
     extend: {
       listStyleType: {
         circle: 'circle',
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
